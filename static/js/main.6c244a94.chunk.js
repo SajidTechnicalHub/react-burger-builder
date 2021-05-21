be71780ea223960b4029e41dac5a2dbb85e6b47d
Sajid Rasheed <sajid.mayoo@gmail.com>
2021-05-22 00:33:08 +0500

    Updates

---
+++ static/js/main.6c244a94.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreactapp=this.webpackJsonpreactapp||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);n(1);var c=n(3),r=n.n(c),s=(n(8),n(0)),i=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"this is my first website deployment"})})};r.a.render(Object(s.jsx)(s.Fragment,{children:Object(s.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.6c244a94.chunk.js.map
